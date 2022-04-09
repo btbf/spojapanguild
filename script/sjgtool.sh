2ddcba7ac7955b271836cf7e711e9f8a33281eb8
BTBF <37224549+btbf@users.noreply.github.com>
2022-04-10 01:26:49 +0400

    Update sjgtool.sh

--- script/sjgtool.sh
+++ script/sjgtool.sh
@@ -10,7 +10,7 @@ cleanup() {
   [[ -n $1 ]] && err=$1 || err=$?
   [[ $err -eq 0 ]] && clear
   tput cnorm # restore cursor
-  [[ -n ${exit_msg} ]] && echo -e "\n${exit_msg}\n" || echo -e "\nGuild LiveView terminated, cleaning up...\n"
+  [[ -n ${exit_msg} ]] && echo -e "\n${exit_msg}\n" || echo -e "\nSPO JAPAN GUILD TOOLを終了しました。\n"
   tput sgr0  # turn off all attributes
   exit $err
 }
